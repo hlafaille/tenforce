f40c501f0fac65f6b2772d663dfb98cdf21362a5
Hunter LaFaille <hlafaille@proton.me>
2023-05-23 00:16:08 -0400

    add LICENSE.md

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from distutils.core import setup
 from Cython.Build import cythonize
 
 setup(
-    name='Tenforce',
-    ext_modules=cythonize(["tenforce/enforcer.pyx"])
+    name='tenforce',
+    ext_modules=cythonize(["tenforce/enforcer.pyx"]),
 )
